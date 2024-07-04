83eeec61602e36218dd26ba5ac615e4bca64fd10
Sammit Badodekar <badodekarsammit@gmail.com>
2024-07-04 20:29:05 +0530

    chore: init shadcn components

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,36 @@
+export { Button, buttonVariants } from "./components/ui/button";
+export {
+    Card,
+    CardHeader,
+    CardFooter,
+    CardTitle,
+    CardDescription,
+    CardContent,
+} from "./components/ui/card";
+export {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuCheckboxItem,
+    DropdownMenuRadioItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuGroup,
+    DropdownMenuPortal,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuRadioGroup,
+} from "./components/ui/dropdown-menu"
+export {
+    Table,
+    TableHeader,
+    TableBody,
+    TableFooter,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableCaption,
+} from "./components/ui/table"
