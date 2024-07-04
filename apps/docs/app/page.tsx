c4f28a44db1b61fdb18d2fa06e0589a311c857c2
Sammit Badodekar <badodekarsammit@gmail.com>
2024-07-04 19:14:38 +0530

    schedulr init

---
+++ apps/docs/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@schedulr/ui/components/ui/button";
+
+export default function Page() {
+  return (
+    <main>
+      schedulr docs
+    </main>
+  );
+}
---
+++ apps/docs/postcss.config.mjs
@@ -0,0 +1,1 @@
+export { default } from "@schedulr/ui/postcss.config";
